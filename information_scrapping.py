63e0a887e55a5af6f7594ce002553404e9711b1b
redneckmike27 <42755894+redneckmike27@users.noreply.github.com>
2018-11-08 18:24:18 -0500

    loop start

--- information_scrapping.py
+++ information_scrapping.py
@@ -8,136 +8,179 @@ def get_html_from_url(url):
     html = html.decode('iso-8859-1')
     return html
 
+def get_pokemon_data_from_html(html):
+    pkm_data = {}
 
-url = "https://www.serebii.net/pokedex-rs/152.shtml"
-pkm_html = get_html_from_url(url)
-pkm_data = {}
+    pointer = parser.getElementsByTagName('p')[0].getChildren()[1].getChildren()[1].getChildren()
 
-parser = AdvancedHTMLParser.AdvancedHTMLParser()
-parser.parseStr(pkm_html)
+    nat_num = pointer[1].innerText.strip()
+    pkm_data['national_number'] = int(nat_num)
 
-pointer = parser.getElementsByTagName('p')[0].getChildren()[1].getChildren()[1].getChildren()
+    hoenn_num = pointer[2].innerText.strip()
+    pkm_data['hoenn_num'] = int(hoenn_num)
 
-nat_num = pointer[1].innerText.strip()
-pkm_data['national_number'] = int(nat_num)
+    pkm_name = pointer[3].innerText.strip()
+    pkm_data['pokemon_name'] = str(pkm_name)
 
-hoenn_num = pointer[2].innerText.strip()
-pkm_data['hoenn_num'] = int(hoenn_num)
+    ability = parser.getElementsByTagName('p')[0].getChildren()[1].getChildren()[2].getChildren()[1].getChildren()[
+        0].innerText.strip()
+    fuck = str(ability).split("&")
 
-pkm_name = pointer[3].innerText.strip()
-pkm_data['pokemon_name'] = str(pkm_name)
+    if len(fuck) == 2:
+        another_fuck = fuck[0].split(" ")
+        pkm_data['ability 2'] = another_fuck[1]
+        ass = fuck[1].strip()
+        fuck = ass
 
-ability = parser.getElementsByTagName('p')[0].getChildren()[1].getChildren()[2].getChildren()[1].getChildren()[0].innerText.strip()
-fuck = str(ability).split("&")
+    pkm_data['ability'] = fuck
 
-if len(fuck) == 2:
-    another_fuck = fuck[0].split(" ")
-    pkm_data['ability 2'] = another_fuck[1]
-    ass = fuck[1].strip()
-    fuck = ass
+    pointer = parser.getElementsByTagName('p')[0].getChildren()[1].getChildren()[5].getChildren()
 
-pkm_data['ability'] = fuck
+    male_percent = pointer[0].innerText.strip().strip("Male: ").strip(" %")
+    # float(male_percent)
+    # male_percent / 100
+    pkm_data['male_percent'] = str(male_percent)
 
-pointer = parser.getElementsByTagName('p')[0].getChildren()[1].getChildren()[5].getChildren()
+    female_percent = pointer[1].innerText.strip().strip("Female: ").strip(" %")
+    pkm_data['female_percent'] = str(female_percent)
 
-male_percent = pointer[0].innerText.strip().strip("Male: ").strip(" %")
-pkm_data['male_percent'] = str(male_percent)
+    pointer = parser.getElementsByTagName('p')[0].getChildren()[1].getChildren()[7].getChildren()
 
-female_percent = pointer[1].innerText.strip().strip("Female: ").strip(" %")
-pkm_data['female_percent'] = str(female_percent)
+    classification = pointer[0].innerText.strip().strip(" Pok&eacute;mon")
+    pkm_data['classification'] = str(classification)
 
-pointer = parser.getElementsByTagName('p')[0].getChildren()[1].getChildren()[7].getChildren()
+    type_1 = pointer[1].getChildren()[0].getChildren()[0]
+    pkm_data['type_1'] = str(type_1)
 
-classification = pointer[0].innerText.strip().strip(" Pok&eacute;mon")
-pkm_data['classification'] = str(classification)
+    type_2 = pointer[2].getChildren()[0].getChildren()[0]
+    pkm_data['type_2'] = str(type_2)
 
-type_1 = pointer[1].getChildren()[0].getChildren()[0]
-pkm_data['type_1'] = str(type_1)
+    height = pointer[3].innerText.strip()
+    pkm_data['height'] = str(height)
 
-type_2 = pointer[2].getChildren()[0].getChildren()[0]
-pkm_data['type_2'] = str(type_2)
+    weight = pointer[4].innerText.strip()
+    pkm_data['weight'] = str(weight)
 
-height = pointer[3].innerText.strip()
-pkm_data['height'] = str(height)
+    next_pkm = parser.getElementsByTagName('p')[0].getChildren()[1].getChildren()[9].getChildren()[0].getChildren()[
+        0].getChildren()[1]
+    net = str(next_pkm)[21:24]
 
-weight = pointer[4].innerText.strip()
-pkm_data['weight'] = str(weight)
+    pointer = parser.getElementsByTagName('p')[1].getChildren()[0].getChildren()[1].getChildren()
 
-next_pkm = parser.getElementsByTagName('p')[0].getChildren()[1].getChildren()[9].getChildren()[0].getChildren()[0].getChildren()[1]
-net = str(next_pkm)[21:24]
+    hold_item = pointer[0].innerText.strip()
+    pkm_data['wild hold item'] = str(hold_item)
 
+    dex_cat = pointer[1]
+    pkm_data['dex category'] = str(dex_cat)
 
+    color_cat = pointer[2].innerText.strip()
+    pkm_data['colour category'] = str(color_cat)
 
-pointer = parser.getElementsByTagName('p')[1].getChildren()[0].getChildren()[1].getChildren()
+    footprint = pointer[3]
+    pkm_data['footprint'] = str(footprint)
 
-hold_item = pointer[0].innerText.strip()
-pkm_data['wild hold item'] = str(hold_item)
+    description = parser.getElementsByTagName('p')[2].getChildren()[0].getChildren()[3].getChildren()[
+        1].innerText.strip()
+    pkm_data['description'] = str(description)
 
-dex_cat = pointer[1]
-pkm_data['dex category'] = str(dex_cat)
+    location = parser.getElementsByTagName('p')[3].getChildren()[1].getChildren()[4].getChildren()[2].innerText.strip()
+    pkm_data['location'] = str(location)
 
-color_cat = pointer[2].innerText.strip()
-pkm_data['colour category'] = str(color_cat)
+    # def
+    # rse_lvl_up_att = {}
 
-footprint = pointer[3]
-pkm_data['footprint'] = str(footprint)
+    # def
+    # tm_hm = {}
 
-description =parser.getElementsByTagName('p')[2].getChildren()[0].getChildren()[3].getChildren()[1].innerText.strip()
-pkm_data['description'] = str(description)
+    # def
+    # tutor_att = {}
 
-location = parser.getElementsByTagName('p')[3].getChildren()[1].getChildren()[4].getChildren()[2].innerText.strip()
-pkm_data['location'] = str(location)
+    egg_steps = parser.getElementsByTagName('p')[11].getChildren()[0].getChildren()[1].getChildren()[
+        0].innerText.strip()
+    pkm_data['egg steps'] = str(egg_steps)
 
-#def
-#rse_lvl_up_att = {}
+    effort_points = parser.getElementsByTagName('p')[11].getChildren()[0].getChildren()[1].getChildren()[
+        1].innerText.strip()
+    pkm_data['effort_points'] = str(effort_points)
 
-#def
-#tm_hm = {}
+    catch_rate = parser.getElementsByTagName('p')[11].getChildren()[0].getChildren()[1].getChildren()[
+        2].innerText.strip()
+    pkm_data['catch rate'] = str(catch_rate)
 
-#def
-#tutor_att = {}
+    egg1 = parser.getElementsByTagName('p')[12].getChildren()[0].getChildren()[2].getChildren()[1].innerText.strip()
+    pkm_data['egg group 1'] = str(egg1)
 
-egg_steps = parser.getElementsByTagName('p')[11].getChildren()[0].getChildren()[1].getChildren()[0].innerText.strip()
-pkm_data['egg steps'] = str(egg_steps)
+    egg_groups_table_length = len(parser.getElementsByTagName('p')[12].getChildren()[0].getChildren())
+    table_lenght = int(egg_groups_table_length)
 
-effort_points = parser.getElementsByTagName('p')[11].getChildren()[0].getChildren()[1].getChildren()[1].innerText.strip()
-pkm_data['effort_points'] = str(effort_points)
+    if table_lenght == 4:
+        egg2 = parser.getElementsByTagName('p')[12].getChildren()[0].getChildren()[3].getChildren()[1].innerText.strip()
+        pkm_data['egg group 2'] = str(egg2)
 
-catch_rate = parser.getElementsByTagName('p')[11].getChildren()[0].getChildren()[1].getChildren()[2].innerText.strip()
-pkm_data['catch rate'] = str(catch_rate)
+    pointer = parser.getElementsByTagName('p')[13].getChildren()[1].getChildren()[2].getChildren()
 
-egg1 = parser.getElementsByTagName('p')[12].getChildren()[0].getChildren()[2].getChildren()[1].innerText.strip()
-pkm_data['egg group 1'] = str(egg1)
+    base_hp = pointer[1].innerText
+    pkm_data['base hp'] = int(base_hp)
 
-egg_groups_table_length = len(parser.getElementsByTagName('p')[12].getChildren()[0].getChildren())
-table_lenght = int(egg_groups_table_length)
+    base_att = pointer[2].innerText
+    pkm_data['base attack'] = int(base_att)
 
-if table_lenght == 4:
-    egg2 =  parser.getElementsByTagName('p')[12].getChildren()[0].getChildren()[3].getChildren()[1].innerText.strip()
-    pkm_data['egg group 2'] = str(egg2)
+    base_def = pointer[3].innerText
+    pkm_data['base defense'] = int(base_def)
 
+    base_sp_att = pointer[4].innerText
+    pkm_data['base sp. attack'] = int(base_sp_att)
 
-pointer = parser.getElementsByTagName('p')[13].getChildren()[1].getChildren()[2].getChildren()
+    base_sp_def = pointer[5].innerText
+    pkm_data['base sp. defense'] = int(base_sp_def)
 
-base_hp = pointer[1].innerText
-pkm_data['base hp'] = int(base_hp)
+    base_speed = pointer[6].innerText
+    pkm_data['base speed'] = int(base_speed)
 
-base_att = pointer[2].innerText
-pkm_data['base attack'] = int(base_att)
-
-base_def = pointer[3].innerText
-pkm_data['base defense'] = int(base_def)
-
-base_sp_att = pointer[4].innerText
-pkm_data['base sp. attack'] = int(base_sp_att)
-
-base_sp_def = pointer[5].innerText
-pkm_data['base sp. defense'] = int(base_sp_def)
-
-base_speed = pointer[6].innerText
-pkm_data['base speed'] = int(base_speed)
-
-
+    return pkm_data
 
 
-print(pkm_data)
+
+if __name__ == "__main__":
+
+    for i in range(1, 101):
+        banks = str(i)
+        if len(banks) == 1:
+            banker = "00"+banks
+
+        if len(banks) == 2:
+            banker = "0"+banks
+
+        if len(banks) == 3:
+            banker = banks
+
+        url = "https://www.serebii.net/pokedex-rs/"+banker+".shtml"
+
+        pkm_html = get_html_from_url(url)
+        pkm_data = {}
+
+        parser = AdvancedHTMLParser.AdvancedHTMLParser()
+        parser.parseStr(pkm_html)
+
+        pkm_data = get_pokemon_data_from_html(pkm_data)
+
+
+        for key in pkm_data:
+            print(key + ': ' + str(pkm_data[key]))
+
+
+
+
+
+    print(bank)
+
+
+
+
+
+
+
+
+
+
+
